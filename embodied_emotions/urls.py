f4783f235b131a89de3a7cc6348f08529fbaf7ea
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-06-16 11:58:47 +0200

    Add index-view for corpus app
    Added view and url-mapping for corpus app. The view now contains a
    Hello World message. This should eventually be replaced by a list of
    available visualizations of the corpus used in the embodied emotions
    project.

--- embodied_emotions/urls.py
+++ embodied_emotions/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'embodied_emotions.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^corpus/', include('corpus.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
