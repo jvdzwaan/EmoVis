dbebf48f8835f50755ba34686312b90c6440d790
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-06-30 14:21:11 +0200

    Add title link subtemplate
    Added a template for title links. Next a template for author links
    should be added.

--- corpus/templatetags/corpus_extras.py
+++ corpus/templatetags/corpus_extras.py
@@ -7,3 +7,11 @@ register = template.Library()
 @register.inclusion_tag('year_link.html')
 def year_link(year):
     return {'year': year}
+
+
+@register.inclusion_tag('title_link.html')
+def title_link(title):
+    return {
+        'ti_id': title.ti_id,
+        'title': title.titel
+    }
