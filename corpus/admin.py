ab2c93a4eb676c5f228775b42dd97bf85d64648c
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-07-04 11:54:13 +0200

    Add Titel, Auteur, Genre, and Subgenre to Django admin module
    In order to see more easily what information on titles is available in
    the database, models Titel, Auteur, Genre, and Subgenre were added to
    the Django admin module.

--- corpus/admin.py
+++ corpus/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from corpus.models import Titel, Auteur, Genre, Subgenre
 
-# Register your models here.
+admin.site.register(Titel)
+admin.site.register(Auteur)
+admin.site.register(Genre)
+admin.site.register(Subgenre)
