3698ad60581e30e92365f641d262695258b6f431
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-08-20 14:30:31 +0200

    Add play metadata to web page with graph
    Play metadata (year, author names, genres, and subgenres) has been
    added to the web page displaying the entity graph.

--- entity_vis/views.py
+++ entity_vis/views.py
@@ -8,7 +8,11 @@ def posemo(request):
 
     context = {
         'title_id': title.ti_id,
-        'title': title.titel
+        'title': title.titel,
+        'year': title.jaar,
+        'authors': [a.voornaam+' '+a.achternaam for a in title.auteurs.all()], 
+        'genres': [genre.genre for genre in title.genres.all()],
+        'subgenres': [subgenre.subgenre for subgenre in title.subgenres.all()]
     }
     
     return render(request, 'entity_vis/index.html', context)
