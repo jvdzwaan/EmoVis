0b41383e6cc4e7b7abc64eb8799a3f93340e398e
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-08-26 16:20:17 +0200

    Add models for entity visualizations
    Added models for storing information about plays, characters, and
    entities in the database. This information will be used for more
    interactive visualizations.
    The next step is to create a script (Django management command) to
    extract information about entities and put it in the database.

--- entity_vis/models.py
+++ entity_vis/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from corpus.models import Titel
+
+class Entity(models.Model):
+    name = models.CharField(max_length=30)
+
+class Character(models.Model):
+    name = models.CharField(max_length=50)
+    play = models.ForeignKey(Titel)
+    num_speaking_turns = models.IntegerField()
+
+class SpeakingTurn(models.Model):
+    play = models.ForeignKey(Titel)
+    character = models.ForeignKey(Character)
+    order = models.IntegerField()
+    entity = models.ManyToManyRel(Entity)
+    score = models.IntegerField()
