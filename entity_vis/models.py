096c4f0e1a4822f93a613f901c62073618a31257
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-08-28 14:54:08 +0200

    Add __unicode__ functions to entity_vis models
    Added __unicode__ functions to entity_vis models, for easy inspection
    in the admin interface.

--- entity_vis/models.py
+++ entity_vis/models.py
@@ -5,13 +5,23 @@ from corpus.models import Titel
 class Entity(models.Model):
     name = models.CharField(max_length=30)
 
+    def __unicode__(self):
+        return self.name
+
 class Character(models.Model):
     name = models.CharField(max_length=50)
     play = models.ForeignKey(Titel)
     num_speaking_turns = models.IntegerField()
+
+    def __unicode__(self):
+        return '{} ({})'.format(self.name, self.play.titel)
 
 class SpeakingTurn(models.Model):
     character = models.ForeignKey(Character)
     order = models.IntegerField()
     entity = models.ManyToManyRel(Entity)
     score = models.IntegerField()
+
+    def __unicode__(self):
+        return '({}) {}'.format(self.order,
+                                      self.character)
