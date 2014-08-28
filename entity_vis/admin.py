10db1f0c8ca7ea399a4b46198864235ab3a1c4ef
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-08-28 11:44:36 +0200

    Add entity_vis models to the admin
    The entity_vis models have been added to the admin for easy inspection
    of the database contents.

--- entity_vis/admin.py
+++ entity_vis/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from entity_vis.models import Entity, Character, SpeakingTurn
 
-# Register your models here.
+
+class CharacterAdmin(admin.ModelAdmin):
+    # There are so many plays (Titels) that it takes forever to build the
+    # ForeignKey drop down, so use raw_id_fields.
+    raw_id_fields = ('play',)
+
+
+admin.site.register(Entity)
+admin.site.register(Character, CharacterAdmin)
+admin.site.register(SpeakingTurn)
