82b61bdee26e98505b30522f1cbc91bc598b1fd2
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-09-01 15:58:29 +0200

    Add moving average function
    Added a function to calculate the moving average given a list of
    numbers. This function is used to smooth e.g. r scores.

--- entity_vis/es.py
+++ entity_vis/es.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """EntityScore helper functions."""
 
+import numpy as np
+
 
 def get_r_score(entity1, entity2):
     """Return the r score for two entities.
@@ -16,3 +18,10 @@ def get_r_score(entity1, entity2):
     if not (score1 == 0 and score2 == 0):
         return (score1-score2)/(score1+score2+0.0)
     return 0.0
+
+
+def moving_average(a, n=3):
+    """Calculate the moving average of array a and window size n."""
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
