828bd42bf70b25ce32c5c3503742f910782215ee
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-09-01 15:45:19 +0200

    Add EntityScore helper function to calculate r scores
    Added a helper function that calculates the r score given two
    EntityScore objects.

--- entity_vis/es.py
+++ entity_vis/es.py
@@ -1,2 +1,18 @@
 # -*- coding: utf-8 -*-
 """EntityScore helper functions."""
+
+
+def get_r_score(entity1, entity2):
+    """Return the r score for two entities.
+    r = (e1-e2)/(e1+e2)
+    """
+    score1 = 0
+    score2 = 0
+    if entity1:
+        score1 = entity1.score
+    if entity2:
+        score2 = entity2.score
+
+    if not (score1 == 0 and score2 == 0):
+        return (score1-score2)/(score1+score2+0.0)
+    return 0.0
