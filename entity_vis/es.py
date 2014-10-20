145ef781c36fbdfd11d58a558d8a33a9cb39c44f
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-10-20 11:41:45 +0200

    Add module for elasticsearch functionality
    Added a module for elasticsearch functionality to the entity_vis app.
    The next step is to add a url mapping and a view for displaying the
    frequency with which entity category words are used.

--- entity_vis/es.py
+++ entity_vis/es.py
@@ -1,27 +1,13 @@
 # -*- coding: utf-8 -*-
-"""EntityScore helper functions."""
+"""ElasticSearch functionality."""
 
-import numpy as np
-
-
-def get_r_score(entity1, entity2):
-    """Return the r score for two entities.
-    r = (e1-e2)/(e1+e2)
-    """
-    score1 = 0
-    score2 = 0
-    if entity1:
-        score1 = entity1.score
-    if entity2:
-        score2 = entity2.score
-
-    if not (score1 == 0 and score2 == 0):
-        return (score1-score2)/(score1+score2+0.0)
-    return 0.0
+from elasticsearch import Elasticsearch
 
 
-def moving_average(a, n=3):
-    """Calculate the moving average of array a and window size n."""
-    ret = np.cumsum(a, dtype=float)
-    ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1:] / n
+def _es():
+    # TODO: make es instance configurable (via settings.py)
+    return Elasticsearch()
+
+
+def search_query(query, doc_type):
+    return _es().search('embodied_emotions', doc_type=doc_type, body=query)
