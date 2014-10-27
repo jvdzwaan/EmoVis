c71cc2edce584fbc9cc9322c32cb9edf17c11579
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-10-27 14:34:20 +0100

    Move base template to general purpose template dir
    A new general purpose directory was created to store project level
    templates in. The base.html template was moved to this directory. This
    was done as preparation of turning the Django application into an
    AngularJS app.

--- embodied_emotions/settings.py
+++ embodied_emotions/settings.py
@@ -91,3 +91,12 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'embodied_emotions', 'static'),
     os.path.join(BASE_DIR, 'bower_components'),
 )
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
