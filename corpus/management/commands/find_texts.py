7f42bcb05ee3fc73112da575dc2d39e76c8903bb
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-10-27 14:07:01 +0100

    Add more titles using title.contains
    Titles that contain exactly one work from the relevant time period are
    added to the title list as well. We still need to find a way to handle
    titles that contain multiple titles.

--- corpus/management/commands/find_texts.py
+++ corpus/management/commands/find_texts.py
@@ -10,6 +10,21 @@ from django.core.management.base import BaseCommand
 from corpus.models import Titel, Genre
 
 
+def get_title(text_id):
+    try:
+        t = Titel.objects.get(pk=text_id)
+    except:
+        t = None
+    return t
+
+
+def get_year(title):
+    m = re.search(r'\d{4}', title.jaar)
+    if m:
+        return int(m.group(0))
+    return None
+
+
 class Command(BaseCommand):
     help = 'Add year data for plays in the corpus to the ElasticSearch index.'
 
@@ -29,17 +44,34 @@ class Command(BaseCommand):
 
         for line in lines:
             text_id = line[0:13]
-            try:
-                t = Titel.objects.get(pk=text_id)
-            except:
-                t = None
+            t = get_title(text_id)
 
-            if(t):
+            if t:
                 if genre_drama in t.genres.all():
-                    m = re.search(r'\d{4}', t.jaar)
-                    year = int(m.group(0))
+                    year = get_year(t)
                     if year <= 1830 and year >= 1600:
                         title_ids[text_id] = None
 
-        #print '# titles found: {}'.format(len(title_ids.keys()))
-        print '\n'.join(title_ids.keys())
+        for line in lines:
+            text_id = line[0:13]
+            t = get_title(text_id)
+
+            if t:
+                if genre_drama in t.genres.all():
+                    if len(t.contains.all()) == 1:
+                        t2 = t.contains.all()[0]
+                        year = get_year(t2)
+
+                        if year and year <= 1830 and year >= 1600:
+                            if t2.ti_id not in title_ids.keys():
+                                title_ids[text_id] = None
+                                print 'Found older version of', t
+                                print t2
+                                print ''
+                    elif len(t.contains.all()) > 1:
+                        print t
+                        for t2 in t.contains.all():
+                            print '\t', t2
+
+        print '# titles found: {}'.format(len(title_ids.keys()))
+        #print '\n'.join(title_ids.keys())
