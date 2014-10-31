a77bed09e34b26acad63b70ce860744219d99e55
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-10-31 16:47:02 +0100

    Retrieve statistics for selected entity categories
    The title id should be read from the url, however, at the moment it is
    set to a certain value.

--- embodied_emotions/static/js/controllers.js
+++ embodied_emotions/static/js/controllers.js
@@ -1,12 +1,19 @@
 var embEmApp = angular.module('embEmApp', []);
 
-embEmApp.controller('EntitiesCtrl', function ($scope, $http){
+// set csrftoken for Django
+embEmApp.config(['$httpProvider', function($httpProvider){
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+}]);
+
+embEmApp.controller('EntitiesCtrl', function ($scope, $http, $location){
     $scope.query = '';
     $scope.results = [];
     $scope.mainCat = '';
     $scope.compareWith = [];
-    var sites_url = 'data/sites.json';
-    $http.get('entity_categories').success(function (data){
+    $scope.statistics = [];
+    $scope.base_url = 'http://'+$location.host()+':'+$location.port();
+    $http.get($scope.base_url+'/entity_vis/entity_categories').success(function (data){
         $scope.categories = data.hits.hits;
         console.log($scope.categories);
     });
@@ -18,17 +25,35 @@ embEmApp.controller('EntitiesCtrl', function ($scope, $http){
     }
     $scope.setMainCat = function(cat) {
         $scope.mainCat = cat;
+        $scope.getEntityStatisticsTitle('bred001moor01');
     }
     $scope.removeMainCat = function() {
         $scope.mainCat = '';
+        $scope.getEntityStatisticsTitle('bred001moor01');
     }
     $scope.addToCompareWith = function(cat) {
         if($scope.compareWith.indexOf(cat) == -1){
             $scope.compareWith.push(cat);
         }
+        $scope.getEntityStatisticsTitle('bred001moor01');
     }
     $scope.removeFromCompareWith = function(cat) {
         var i = $scope.compareWith.indexOf(cat);
         $scope.compareWith.splice(i, 1);
+        $scope.getEntityStatisticsTitle('bred001moor01');
+    }
+    $scope.getEntityStatisticsTitle = function(title_id){
+        var url = $scope.base_url+'/corpus/entity_stats/'+title_id+'/';
+        if($scope.mainCat){
+            var categories = [$scope.mainCat].concat($scope.compareWith);
+        } else {
+            var categories = $scope.compareWith;
+        }
+        console.log(categories);
+        $http.post(url, {categories: categories}).
+            success(function (data){
+                console.log(data);
+                $scope.statistics = data;
+        });
     }
 });
